f14bcac6bcfe7631f52a352f5e0d71a2bc002e7d
lokeCodes <125755158+lokeCodes@users.noreply.github.com>
2023-02-22 20:59:36 +0530

    Add files via upload

---
+++ INCRIQ.java
@@ -0,0 +1,18 @@
+// https://www.codechef.com/problems/INCRIQ
+// Question Link^^^
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int x = 7+(s.nextInt());
+		String res = (x>170) ? "YES" : "NO";
+		System.out.println(res);
+	}
+}
