c3bfee5b98ca5fddc929580ee40aef63afff8565
lokeCodes <125755158+lokeCodes@users.noreply.github.com>
2023-02-22 20:38:51 +0530

    Add files via upload

---
+++ INSURANCE.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		while(t-- > 0){
+		    int x = s.nextInt();
+		    int y = s.nextInt();
+		    int z = Math.min(x,y);
+		    if(x==y || y==x){
+		        System.out.println(x);
+		    }else{
+		        System.out.println(z);
+		    }
+		}
+	}
+}
