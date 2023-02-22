d20176f8a5b9bb359c41b32e68add43416ee8822
lokeCodes <125755158+lokeCodes@users.noreply.github.com>
2023-02-22 20:32:43 +0530

    Add files via upload

---
+++ COURSEREG.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		while(t-- > 0){
+		    int a = s.nextInt();
+		    int b = s.nextInt();
+		    int c = s.nextInt();
+		    int res = a+c;
+		    String res1 = (res<=b) ? "YES" : "NO";
+		    System.out.println(res1);
+		}
+	}
+}
